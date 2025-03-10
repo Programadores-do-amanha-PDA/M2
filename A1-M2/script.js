edc3d4aa2e7cee4090a9ea6c2f1184b8c6243f92
Bia-source <bia_ferreirad@yahoo.com>
2025-03-10 19:46:11 -0300

    revisão javascript e es6

---
+++ A1-M2/script.js
@@ -0,0 +1,53 @@
+// o que é uma funçao
+// um bloco de código que executa/faz algo
+
+//               parametros
+function somarValores(valor1, valor2){
+  var resultado = valor1 + valor2
+  // 7
+  return resultado
+}
+
+// console.log(somarValores(3, 50))
+// console.log(somarValores(32, 50))
+// console.log(somarValores(34, 50))
+// console.log(somarValores(3965, 50))
+
+// o que são loops?
+// repetição -> estruturas de repetição
+// for -> para executar um código varias vezes
+// for -> para percorrer um array/ olhar todos os valores de um array
+
+// array -> []
+// 
+//posição/index/indice/SENHA 0         1       2        3 
+var numeros = ["beatriz", "tabs", "davi", "carlos"];
+// dahsodhioa(['dbisai', 'nsalnd'])
+// // ["d", "n"]
+// variavel de incremento/contador
+// condição para parar o loop
+// incremento
+// for(var i = 0; i < numeros.length; i++){
+//  console.log(numeros[i]);
+// }
+
+// LET -> defini uma variavel local e flexivel
+function exibirNome(){
+    let nome = "Joao"
+    nome = "duda"
+    console.log(nome);
+}
+
+exibirNome();
+console.log("variavel nome fora da funcao: " + nome)
+
+// CONST -> defini uma variavel que nao irá mudar
+const cpfBeatriz = "123.456.789-00"
+const perfumes = [ 
+    {nome: "boticario1", preco: 1234},
+    {nome: "boticario2", preco: 1236},
+    {nome: "boticario3", preco: 1237},
+    {nome: "boticario4", preco: 1238},
+]
+
+// perfumes[0].nome = "teste";
