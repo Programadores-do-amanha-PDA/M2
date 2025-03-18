b21df74af3d9c88ee637604583a452d4cab90825
Bia-source <bia_ferreirad@yahoo.com>
2025-03-18 19:25:47 -0300

    explicacao de objetos

---
+++ A5-M2/script.js
@@ -0,0 +1,27 @@
+// OBJETOS -> coisas que utilizamos no dia a dia
+// coisas que podemos tocar/ algo palpavel 
+
+// OBJETOS PROGRAMAÇÃO -> trazer para o código algo que já existe no mundo real
+// OBJETOS PROGRAMAÇÃO -> representação de algo da vida real
+
+// const pessoa = () => {// bloco de código}
+// objeto pessoa {}
+const pessoa = {
+    cpf: "123.123.123-88",
+    nome: "Beatriz",
+    idade: 24
+} 
+
+// ERRADO
+// const nomeProduto = 'telefone';
+// const precoProduto = 1200;
+// const descricao = "telefone sem fio";
+
+const produto = {
+    nome: 'telefone',
+    preco: 1200,
+    descricao: "telefone sem fio",
+    img: "telefone.png"
+}
+
+
