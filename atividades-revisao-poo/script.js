760efe9bbcabc59c66d03ddedc570faf14d93981
Bia-source <bia_ferreirad@yahoo.com>
2025-03-31 20:26:02 -0300

    criando uma classe nivel 3

--- atividades-revisao-poo/script.js
+++ atividades-revisao-poo/script.js
@@ -10,9 +10,72 @@ var ouvinte = {
 // CLASSE -> Som
 // ATRIBUTOS -> volume
 
+class Som{
+    constructor(volume){
+        this.volume = volume;
+    }
+}
+
+const mp3 = new Som(13);
+mp3.volume // 13
+
 // verificar quem poderia ter aquelas caracteristicas
 // volume é uma caracteristica DE QUEM? DO SOM
 
 // Tarefa:
 // Crie uma estrutura para representar essa informação.
 // Implemente um método que permita aumentar o volume.
+
+var restaurante = {
+  funcionarios: [],
+  cardapio: [],
+  clientes: [],
+  formaPagamento: [],
+  pedidos: []
+}
+
+const carro = {
+    roda: 4,
+    nome: "",
+    marca: "",
+    modelo: ""
+}
+
+
+// o passageiro solicita uma VIAGEM - []
+// o motorista aceita uma VIAGEM - []
+// o app gerencia as VIAGENS - []
+
+// o cliente busca um PRODUTO
+// o funcionario vende um PRODUTO
+// o carrinho adiciona um PRODUTO
+
+const musica = {
+    genero: "",
+    nome: "",
+    album: "",
+    artista: "",
+    tempo: 180,
+    anoLancamento: 2020
+}
+
+const veiculo = {
+    nome: "",
+    marca: "",
+    placa: "",
+    ano: 2020,
+    dono: "bea",
+    combustivel: "flex"
+}
+
+const curso = {
+    nome: "",
+    cargaHoraria: 200,
+    categoria: "matematica",
+    objetivo: "este curso é para desnvolvimento",
+    preco: 140,
+    quantidadeAulas: 30,
+    quantidadeModulos: 3
+}
+
+
