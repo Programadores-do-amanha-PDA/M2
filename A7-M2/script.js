233bb22d79ea03dd4b25570d536e41a4523d434e
Bia-source <bia_ferreirad@yahoo.com>
2025-04-07 20:48:04 -0300

    adicionando interatividade com o usuario a requisicao

--- A7-M2/script.js
+++ A7-M2/script.js
@@ -68,24 +68,49 @@ async function buscarInfoPokemon(){
 //     imgPokemon.src = pokemon.sprites.front_default;
 // }
 
-async function buscar2(){
-    const resultadoBuscaApi = await fetch("https://pokeapi.co/api/v2/pokemon/charmander");
+// trocar de imagem com clicks
+// o usuario digitar o nome do personagem que quer
+
+// em 10min voces irão criar um evento onde
+// ao passar o mouse encima da imagem 
+// troca para outra imagem 
+// quando passar o mouse encima saia de pokemon.sprites.front_default;
+// para pokemon.sprites.front_shiny;
+
+async function buscar(inputPokemon){
+    const resultadoBuscaApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputPokemon}`);
     const pokemon = await resultadoBuscaApi.json();
 
     const nomePokemon = document.getElementById("nome-pokemon");
     nomePokemon.innerText = pokemon.name;
 
-    const imgPokemon = document.getElementById("img-pokemon");
-    imgPokemon.src = pokemon.sprites.front_default;
+    const imgPokemon1 = document.getElementById("img-pokemon1");
+    const imgPokemon2 = document.getElementById("img-pokemon2");
+    const imgPokemon3 = document.getElementById("img-pokemon3");
+    const imgPokemon4 = document.getElementById("img-pokemon4");
+    const imgPokemon5 = document.getElementById("img-pokemon5");
 
-    imgPokemon.style.height = "350px";
-    imgPokemon.style.width = "350px";
+    imgPokemon1.src = pokemon.sprites.front_default;
+    imgPokemon2.src = pokemon.sprites.back_default;
+    imgPokemon3.src = pokemon.sprites.back_shiny;
+    imgPokemon4.src = pokemon.sprites.front_shiny;
+    imgPokemon5.src = pokemon.sprites.front_default;
 }
 
-buscar2()
+const inputNamePokemon = document.getElementById("input-name-pokemon");
+const btnBuscarPokemon = document.getElementById("btn-buscar");
+
+// quando clicar no botão pegar o VALOR do inputNamePokemon 
+// e inserir esse valor no ARGUMENTO da função buscar
+btnBuscarPokemon.addEventListener("click", ()=> {
+  buscar(inputNamePokemon.value);
+});
+
 // getInfoPokemon();
 
 // criar uma função que faça uma requisição para a api de pokemon 
 // essa função deve receber as informações do pokemon e alterar 
 // o html 
 // OBS: quero ver todas as imagens do pokemon e o nome dele 
+
+// quando passa o mouse encima muda para virar de lado
