a345cbe704c77b24afe5f853a06c083149ac1436
Bia-source <bia_ferreirad@yahoo.com>
2025-04-14 20:46:10 -0300

    explicacao moment.js

--- A11-M2/script.js
+++ A11-M2/script.js
@@ -14,7 +14,7 @@
 const newDate = new Date();
 
 const date = document.getElementById("date");
-date.innerText = newDate;
+// date.innerText = newDate;
 // 04/12/2023
 
 // se for domingo coloque na tela 
@@ -39,3 +39,47 @@ const message = {
 if((newDate.getMinutes() - message.createdAt.getMinutes) < 15){
     console.log("tempo de edição excedido")
 }
+
+
+// Biblioteca -> lugar onde tem varios livros armazenados
+
+// Biblioteca na Programação -> representa um livro com varios recursos
+
+// Biblioteca CALCULAR2.0 -> somar, subtrair, multiplicar e dividir
+
+function novaData(){
+    return moment().format('DD [de] MMMM [de] YYYY as HH:mm');
+}
+
+// função diff trabalha com intervalos entre datas
+// ou calcula a diferença entre datas
+function quantosAnos(){
+    const hoje = moment();
+    //                         ano-dia-mes
+    const nascimento = moment('2000-04-05')
+    const idade = hoje.diff(nascimento,'hours');
+    return idade;
+}
+
+// DD/MM/YYYY
+date.innerText = novaData();
+
+// function novaData() {
+//     const hoje = new Date();
+//     let dia = hoje.getDate();
+//     let mes = hoje.getMonth() + 1; // Janeiro é 0
+//     const ano = hoje.getFullYear();
+
+//     if (dia < 10) dia = '0' + dia;
+//     if (mes < 10) mes = '0' + mes;
+
+//     return dia + '/' + mes + '/' + ano;
+//   }
+//   console.log(novaData());
+
+  document.getElementById('data').innerText = novaData();
+
+
+  // pensem na black friday e calculem quantos meses faltam para a black friday 
+  // utilizando o momento 
+  // "faltam x meses para black friday"
