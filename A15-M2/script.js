d6f8ac2e01c94b5494ba61347422ae79c1880404
Bia-source <bia_ferreirad@yahoo.com>
2025-04-28 20:57:47 -0300

    explicacao map e exercicios

--- A15-M2/script.js
+++ A15-M2/script.js
@@ -109,4 +109,43 @@ if(almocou === true){
 const result = (almocou === true) ? "vou comer a sobremesa": "nao vai comer a sobremesa";
 console.log(result);
 
+// MÉTODOS DO ARRAY
+const nomes = [];
 
+// Push adiciona um elemento no final do array
+nomes.push("Davi");
+console.log(nomes)
+// unshift / shift / pop
+
+// MÉTODOS DE VISUALIZAÇÃO
+// Map -> criar um novo array e executar o mesmo código para TODOS OS ELEMENTOS
+const numeros = [1,2,3,4,5,6];
+const qualNumero = numeros.map(numero => `numero: ${numero}`)
+//callbackfn -> usar uma outra função como parâmetro
+console.log(qualNumero)
+
+const carrinhoShein = [100,250,60,400];
+const carrinhoSheinComDesconto = carrinhoShein.map(preco => preco - (preco * 0.10));
+console.log(carrinhoSheinComDesconto);
+
+const nomeAlunos = ['Erick',  'Luis', 'Nyvea', 'Luana'];
+const nomesMaiusculos = nomeAlunos.map(aluno => aluno.toUpperCase())
+console.log(nomesMaiusculos)
+
+const produtos = [
+    {nome: "batata", preco: 3.00},
+    {nome: "feijão", preco: 15.00},
+    {nome: "salada", preco: 1.50},
+]
+
+const apresentacaoProdutos = produtos.map(produto => `${produto.nome} sai por apenas ${produto.preco}`);
+console.log(apresentacaoProdutos)
+
+// Dada uma lista de palavras, use map para transformar todas em letras maiúsculas.
+// frutas = ['banana', 'maçã', 'laranja', 'uva']
+
+// Dada uma lista de palavras, use map para gerar uma nova lista com o número de letras de cada palavra.
+// animais = ['cachorro', 'gato', 'elefante', 'leão']
+
+// Dada uma lista de números, use map para transformá-los em multiplos de 3
+// numeros = [10, 20, 30, 40]
